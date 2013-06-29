c36e0c191607d0d3edc4ceb6f27d2deed67fe1a6
plamenyovchev <plamenkooo@gmail.com>
2013-06-30 00:19:37 +0300

    Bulls and cows code
    not finished

---
+++ BullsAndCows.Client/Scripts/controller.js
@@ -0,0 +1,161 @@
+﻿/// <reference path="http-Requester.js" />
+/// <reference path="persisters.js" />
+/// <reference path="prototype.js" />
+/// <reference path="jquery-2.0.2.js" />
+
+var controllers = (function () {
+
+    var rootUrl = "http://localhost:40643/api/";
+
+    var Controller = Class.create({
+        initialize: function () {
+            this.persister = persisters.get(rootUrl);
+        },
+
+        loadUI: function (selector) {
+            if (this.persister.isLoggedIn()) {
+                this.loadGameUI(selector);
+            } else {
+                this.loadLoginFormUI(selector);
+            }
+
+            this.attachUIEventHandlers(selector);
+        },
+
+        loadLoginFormUI: function (selector) {
+            var loginForm =
+                '<form>'
+                       + ' <label for="#tb-login-username">Username: </label>    ' +
+                       ' <input type="text" id="tb-login-username" />          ' +
+                       ' <label for="#tb-login-password">Password: </label>    ' +
+                       ' <input type="text" id="tb-login-password" />          ' +
+                       ' <button id="login-button">Login</button>' +
+                '</form>';
+            $(selector).html(loginForm);
+        },
+
+        loadGameUI: function (selector) {
+            var html =
+                        '<span id="user-nickname">' +
+                        this.persister.getNickname() +
+                        '</span>' +
+                        '<button id="logout-button">Logout</button>' +
+                        '<h2>Open Games</h2>                       ' +
+                        '<div id="open-games-container"></div>     ' +
+                        '<h2>Active Games</h2>                     ' +
+                        '<div id="active-games-container"></div>';
+
+            $(selector).html(html);
+
+            this.persister.game.open(function (games) {
+                var list = '<ul>';
+                var i = 0;
+                var gamesLen = games.length;
+
+                for (i; i < gamesLen; i += 1) {
+                    var game = games[i];
+
+                    list +=
+                         '<li data-game-id="' + game.id + '">' +
+                             '<a href="#">' +
+                                 game.title +
+                              '</a>' +
+                            '<span> by ' +
+                                game.creatorNickname +
+                            '</span>' +
+                         '</li>';
+                }
+
+                list += '</ul>';
+                $(selector + " #open-games-container").html(list);
+            });
+
+            this.persister.game.myActive(function (games) {
+                var list = '<ul>';
+                var i = 0;
+                var gamesLen = games.length;
+
+                for (i; i < gamesLen; i += 1) {
+                    var game = games[i];
+
+                    list +=
+                        '<li data-game-id="' + game.id + '">' +
+                            '<a href="#">' +
+                                $("<div />").html(game.title).text() + //hack escaping ;)
+                            '</a>' +
+                        '</li>';
+                }
+
+                list += '</ul>';
+                $(selector + " #active-games-container").html(list);
+            });
+        },
+
+        attachUIEventHandlers: function (selector) {
+            var self = this;
+            var wrapper = $(selector);
+            wrapper.on('click', "#login-button", function () {
+                var user = {
+                    username: $(selector + " #tb-login-username").val(),
+                    password: $(selector + " #tb-login-password").val()
+                }
+
+                self.persister.user.login(user, function () {
+                    self.loadGameUI(selector);
+                }, function () {
+                    self.loadLoginFormUI(selector);
+                });
+                return false; //!!! IMPORTANT
+            });
+
+            wrapper.on('click', "#register-button", function () {
+
+            });
+
+            wrapper.on('click', "#logout-button", function () {
+                self.persister.user.logout(function () {
+                    self.loadLoginFormUI(selector);
+                });
+            });
+
+            wrapper.on('click', "#open-games-container a", function () {
+                $("#game-join-inputs").remove();
+                var html =
+                    '<div id="game-join-inputs">' +
+                        'Number: <input type= "text" id = "game-number" />' +
+                        'Password: <input type= "text" id = "game-password" />' +
+                        '<button id="join-game-button">Join</button>' +
+                    '</div>';
+
+                $(this).after(html);
+            });
+
+            wrapper.on('click', "#join-game-button", function () {
+                var game = {
+                    number: $("#game-number").val(),
+                    gameId: $(this).parents("li").first().data("game-id")
+                };
+
+                var password = $("#game-password").val();
+                if (password) {
+                    game.password = password;
+                }
+
+                self.persister.game.join(game);
+            });
+        }
+    });
+
+    return {
+        get: function () {
+            return new Controller();
+        }
+    }
+
+})();
+
+//on document ready
+$(function () {
+    var controller = controllers.get();
+    controller.loadUI("#wrapper");
+});
---
+++ BullsAndCows.Client/Scripts/persisters.js
@@ -0,0 +1,206 @@
+﻿/// <reference path="jquery-2.0.2.js" />
+/// <reference path="prototype.js" />
+/// <reference path="http-Requester.js" />
+/// </// <reference path="http://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/sha1.js"/>
+/// <reference path="sha1.js" />
+
+var persisters = (function () {
+    
+    var nickname = localStorage.getItem("nickname");
+    var sessionKey = localStorage.getItem("sessionKey");
+
+    function saveUserData(userData) {     
+        localStorage.setItem("nickname", userData.nickname);
+        localStorage.setItem("sessionKey", userData.sessionKey);
+        nickname = userData.nickname;
+        sessionKey = userData.sessionKey;
+    }
+
+    function clearUserData() {
+        localStorage.removeItem("nickname");
+        localStorage.removeItem("sessionKey");
+
+        localStorage.setItem("nickname", "");
+        localStorage.setItem("sessionKey", "");
+        nickname = "";
+        sessionKey = "";
+    }
+
+    var MainPersister = Class.create({
+
+        initialize: function (rootUrl) {
+            this.rootUrl = rootUrl;
+            this.user = new UserPersister(this.rootUrl);
+            this.game = new GamePersister(this.rootUrl);
+        },
+
+        isLoggedIn: function () {
+            var isLogged = nickname != null && sessionKey != "";
+            return isLogged;
+        }
+        ,
+        getNickname: function () {
+            return nickname;
+        }
+    });
+
+
+    var UserPersister = Class.create({
+        initialize: function (rootUrl) {
+            this.rootUrl = rootUrl + "user/";
+        },
+
+        login: function (user, success, error) {
+            var url = this.rootUrl + "login";
+            var userData = {
+                username: user.username,
+                authCode: CryptoJS.SHA1(user.username + user.password).toString() //crypting userData!
+            };
+            // post user data(login)
+            httpRequester.postJSON(url, userData,
+                function (data) {
+                    saveUserData(data);
+                    success(data);
+                }, error);
+        },
+
+        register: function (user, data, success, error) {
+            var url = this.rootUrl + "register";
+            var userData = {
+                username : user.username,
+                nickname : user.nickname,
+                authCode: CryptoJS.SHA1(user.username + user.password).toString()
+            }
+            //post user data(register)
+            httpRequester.postJSON(url, userData,
+                function (data) {
+                    saveUserData(data);
+                    success(data);
+                }, error);
+        },
+
+        logout: function (success, error) {
+            var url = this.rootUrl + "logout/" + sessionKey;
+            httpRequester.getJSON(url, function (data) {
+                clearUserData();
+                success(data);
+            }, error);
+        },
+
+        scores: function (success, error) {
+
+        }
+    });
+
+    var GamePersister = Class.create({
+        
+        initialize: function (url) {
+            this.rootUrl = url + "game/";
+        },
+
+        create: function (game,success,error) {
+            var url = this.rootUrl + "create/" + sessionKey;
+            
+            httpRequester.postJSON(url, game, success, error);
+        },
+
+        join: function (game,success,error) {
+            var url = this.rootUrl + "join/" + sessionKey;
+
+            httpRequester.postJSON(url, game, success, error);
+        },
+
+        start: function () {
+
+        },
+
+        myActive: function (success,error) {
+            var url = this.rootUrl + "my-active/" + sessionKey;
+            httpRequester.getJSON(url, success, error);
+        },
+
+        open: function (success,error) {
+            var url = this.rootUrl + "open/"+ sessionKey;
+            httpRequester.getJSON(url, success, error);
+        },
+
+        state: function () {
+
+        }
+
+    });
+
+
+    var GuessPersister = Class.create({
+
+        intialize: function () {
+
+        },
+
+        make: function () {
+
+        }
+
+    });
+
+    var MessagesPersister = Class.create({
+
+        initilize: function () {
+
+        },
+
+        unread: function () {
+
+        },
+
+        all: function () {
+
+        }
+
+    });
+
+    return {
+        get: function (url) {
+            return new MainPersister(url);
+        }
+    }
+})();
+
+var bullsAndCowsPersister = persisters.get("http://localhost:40643/api/");
+
+/* TEST USER LOGIN */
+
+//var user = {
+//    username: "Joro",
+//    password: "joro"
+//}
+
+//bullsAndCowsPersister.user.login(user,
+//    function (data) {
+//        alert(JSON.stringify(data));
+//    }, function (err) {
+//        alert(JSON.stringify(err));
+//    });
+
+/* END TEST USER LOGIN */
+
+/* TEST USER REGISTER */
+
+//var user = {
+//    username: "Joro2",
+//    nickname: "Gerogi2",
+//    password: "joro2"
+//}
+
+//bullsAndCowsPersister.user.register(user,
+//    function (data) {
+//        alert(JSON.stringify(data));
+//    }, function (err) {
+//        alert(JSON.stringify(err));
+//    });
+
+/* END TEST USER REGISTER */
+//http://localhost:40643/api/user/login
+//http://localhost:40643/api/game/create
+//http://localhost:40643/api/game/join
+//http://localhost:40643/api/messages/all
